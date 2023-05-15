4bf26165ec6410a7e36c9396accd09ddde7d7a9a
chc72331 <chc72331@creighton.edu>
2023-05-15 11:24:18 -0500

    Update repo_upload_test.py

--- repo_upload_test.py
+++ repo_upload_test.py
@@ -2,13 +2,7 @@
 
 # changes made today
 
-new_var = 9
 
-new_word = "just playing around"
 
-''' grey, do you see this????'''
-print("Grey, this is your pilot speaking.")
+
  
- # 2nd edit from VS vode
-
-
