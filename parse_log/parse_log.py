e202f3f8798ae13b1de384a1483f9f5faf359169
chc72331 <chc72331@creighton.edu>
2024-03-18 14:30:41 -0500

    Initial code

--- parse_log/parse_log.py
+++ parse_log/parse_log.py
@@ -1,4 +1,21 @@
 import re
 import pandas as pd
 
-selected_file = 
+selected_file = "dummy.log"
+
+working_file = open(selected_file)
+
+'''
+with open(selected_file) as f:
+    f = f.readlines()
+
+for line in f:
+    for phrase in keep_phrases:
+        if phrase in line:
+            important.append(line)
+            break
+'''
+
+print(working_file.read())
+
+working_file.close()
